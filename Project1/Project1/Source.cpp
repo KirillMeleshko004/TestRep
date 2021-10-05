30e991c8e9a19f3f53788ea5719a9c0e1c603057
KirillMeleshko004 <meleshkokirill004@gmail.com>
2021-10-05 20:15:23 +0300

    HelloWorld ver.2
    Additionally give us sum of two numbers

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
 int main()
-{
+{	
 	std::cout << "Hello world!" << std::endl;
+	std::cout << "Enter two numbers" << std::endl;
+	double a, b;
+	std::cin >> a >> b;
+	std::cout << "a + b = " << a + b << std::endl;
 }
